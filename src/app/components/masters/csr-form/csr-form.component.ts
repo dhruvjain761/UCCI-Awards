0c08a40589994c553a1c1aa08b2c4a39b56045c8
dhruvjain761 <dhruvjain3d3@gmail.com>
2022-11-21 13:43:11 +0530

    Changes

--- src/app/components/masters/csr-form/csr-form.component.ts
+++ src/app/components/masters/csr-form/csr-form.component.ts
@@ -19,8 +19,8 @@ export class CsrFormComponent implements OnInit {
   sections: any = [];
   position: string;
   formData: any = {};
-  previewCheck: any = false;
-  actionString: string = ''
+  previewCheck: any = true;
+  actionString: string = '';
   breadcrumb: any[] = [
     {
       title: '',
@@ -38,7 +38,7 @@ export class CsrFormComponent implements OnInit {
     private _formBuilder: FormBuilderService,
     private messageService: MessageService,
     private confirmationService: ConfirmationService
-  ) { }
+  ) {}
 
   responseMessage: boolean = false;
   award_form_id: any;
@@ -52,6 +52,8 @@ export class CsrFormComponent implements OnInit {
         console.log(res);
         this.spinner.hide();
         this.responseMessage = res?.data[0]?.form_response ? true : false;
+        console.log(this.sections);
+
         if (res?.data[0]?.form_json) {
           this.sections = JSON?.parse(JSON?.parse(res?.data[0]?.form_json));
           this.breadcrumb[0].title = res?.data[0].form_title;
@@ -71,8 +73,7 @@ export class CsrFormComponent implements OnInit {
           } else if (res?.data[0]?.form_title === 'CSR Award') {
             this.hindiTitle = 'सामाजिक उत्तरदायित्व पुरस्कार';
           }
-        }
-        else if (res?.data[0]?.form_response) {
+        } else if (res?.data[0]?.form_response) {
           this.sections = JSON?.parse(JSON?.parse(res?.data[0]?.form_response));
           this.breadcrumb[0].title = res?.data[0]?.form_title;
           this.formId = res?.data[0]?.id;
@@ -81,18 +82,17 @@ export class CsrFormComponent implements OnInit {
 
           if (res?.data[0]?.form_title === 'Service Sector Award') {
             this.hindiTitle = 'सेवा क्षेत्र पुरस्कार';
-          }
-          else if (res?.data[0]?.form_title === 'Manufacturing Sector Award') {
+          } else if (
+            res?.data[0]?.form_title === 'Manufacturing Sector Award'
+          ) {
             this.hindiTitle = 'विनिर्माण क्षेत्र पुरस्कार';
-          }
-          else if (res?.data[0]?.form_title === 'Social Enterprises Award') {
+          } else if (res?.data[0]?.form_title === 'Social Enterprises Award') {
             // debugger;
             this.hindiTitle = 'सामाजिक उपक्रम पुरस्कार';
-          }
-          else if (res?.data[0]?.form_title === 'CSR Award') {
+          } else if (res?.data[0]?.form_title === 'CSR Award') {
             this.hindiTitle = 'सामाजिक उत्तरदायित्व पुरस्कार';
           }
-
+          console.log(this.sections);
           // this.sections.forEach((section:any) => {
           //   section.controls.forEach((control:any) => {
           //     if(control.name == 'checkbox') {
@@ -100,11 +100,10 @@ export class CsrFormComponent implements OnInit {
           //     }
           //   });
           // });
-          
         }
       });
 
-    console.log(this.title, this.hindiTitle)
+    console.log(this.title, this.hindiTitle);
 
     let date = new Date();
 
@@ -135,7 +134,7 @@ export class CsrFormComponent implements OnInit {
   }
 
   getFormResponse(event: any) {
-    console.log(event)
+    console.log(event);
     if (event?.string == 'save draft') {
       let Object = {
         award_form_id: this.award_form_id,
@@ -159,11 +158,11 @@ export class CsrFormComponent implements OnInit {
             detail: res.message,
           });
         });
-    }
-    else if (event?.string == 'submit') {
-      this.previewCheck = true;
+    } else if (event?.string == 'submit') {
+      // this.previewCheck = true;
       this.confirmationService.confirm({
-        message: 'Are you sure you want to Submit? You would not be able to Edit once Submitted.',
+        message:
+          'Are you sure you want to Submit? You would not be able to Edit once Submitted.',
         header: 'Confirmation',
         icon: 'pi pi-exclamation-triangle',
         accept: () => {
@@ -171,7 +170,7 @@ export class CsrFormComponent implements OnInit {
           let Object = {
             award_form_id: this.award_form_id,
             form_response: JSON.stringify(event?.Obj),
-            status: 'completed'
+            status: 'completed',
           };
 
           if (this.responseMessage) {
@@ -191,14 +190,13 @@ export class CsrFormComponent implements OnInit {
                 detail: res.message,
               });
             });
-        }
+        },
       });
-    }
-    else if (event?.string == 'not valid') {
+    } else if (event?.string == 'not valid') {
       this.previewCheck = true;
       this.messageService.add({
         severity: 'error',
-        detail: 'Please fill required fields'
+        detail: 'Please fill required fields',
       });
     }
   }
